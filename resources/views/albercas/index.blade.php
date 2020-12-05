56863bc79d6b50191bfa4f4163f51a4c4cd11fe3
andre-lazo <andredaniellazovillanueva@gmail.com>
2020-12-04 23:29:27 -0500

    cambio js

--- resources/views/albercas/index.blade.php
+++ resources/views/albercas/index.blade.php
@@ -14,7 +14,7 @@
       <script src="{{asset('fullcalendar/interaction/main.js')}}" defer></script>
       <script src="{{asset('fullcalendar/daygrid/main.js')}}" defer></script>
       <script src="{{asset('fullcalendar/list/main.js')}}" defer></script>
-      <script src="{{asset('fullcalendar/timegrid /main.js')}}" defer></script>
+      <script src="{{asset('fullcalendar/timegrid/main.js')}}" defer></script>
       <script>
          
           var url_show="{{url('/albercas/show')}}"; 
